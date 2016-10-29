4356ca97a33b81772f026e52025aafedc86c0536
Rolando <rolo_xo@outlook.com>
2016-10-28 22:52:18 -0500

    Initial commit

---
+++ app/src/main/java/com/rolando/practicanfc/MainActivity.java
@@ -0,0 +1,42 @@
+package com.rolando.practicanfc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.nfc.NfcAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "NfcDemo";
+
+    private TextView mTextView;
+    private NfcAdapter mNfcAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.textView_explanation);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (mNfcAdapter == null) {
+
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+
+        }
+
+        if (!mNfcAdapter.isEnabled()) {
+            mTextView.setText("NFC is Disabled.");
+        } else {
+            mTextView.setText("NFC is Enabled");
+        }
+    }
+    }
+
